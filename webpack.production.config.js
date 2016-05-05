bd9ebb805bde145843fbbffe00f30457058f274d
Gordeev <andrey@192.168.0.101.ipl.orionnet.ru>
2016-05-05 22:05:11 +0700

    chore(*): production config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,22 +2,32 @@ var path = require('path');
 var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
 var precss = require('precss');
+var StatsPlugin = require('stats-webpack-plugin');
 var postcssImport = require('postcss-import');
 
 module.exports = {
-  devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
+    path.join(__dirname, 'src/index.js')
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+    new StatsPlugin('webpack.stats.json', {
+      source: false,
+      modules: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
   ],
   module: {
     loaders: [
@@ -38,5 +48,5 @@ module.exports = {
       precss,
       autoprefixer
     ];
-  }
+  }  
 };
